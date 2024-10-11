3b1c32f767b3e2c2998ca229425a0b6e8f416851
Wiley Bennett <wiley@bmdware.com>
2024-10-10 20:53:24 -0700

    visuals

--- src/client/LogOut.tsx
+++ src/client/LogOut.tsx
@@ -10,7 +10,7 @@ export default function LogOut() {
     }, []);
     return (
         <button onClick={clearCache}>
-            <span>Delete analysis</span>
+            <span>Finish analysis</span>
         </button>
     );
 }
